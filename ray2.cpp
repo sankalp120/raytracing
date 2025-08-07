0c9625bba4eb847287c8c26a3b69b7e43cb8b872
sankalp120 <sankalpjhamb10@gmail.com>
2025-08-07 20:21:27 +0530

    Added output indicator

--- ray2.cpp
+++ ray2.cpp
@@ -9,9 +9,8 @@ int main() {
 
     // Render
 
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
-    for (int j = 0; j < image_height; j++) {
+    for (int j = 0; j < image_height; ++j) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++) {
             auto r = double(i) / (image_width-1);
             auto g = double(j) / (image_height-1);
@@ -24,4 +23,6 @@ int main() {
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+
+    std::clog << "\rDone.                 \n";
 }
